924d0181b9d66dad519b6bf69af143088f84d567
Filipe Leuch Bonfim <filipe1309@gmail.com>
2024-09-15 18:08:00 -0300

    feat(section-5): building the routes, handlers, and email templates

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,20 +4,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 const webPort = "80"
 
-type Config struct {}
+type Config struct {
+	Mailer Mail
+}
 
 func main() {
-	app := Config{}
+	app := Config{
+		Mailer: createMail(),
+	}
 
 	log.Printf("Starting mail service on port %s\n", webPort)
 
 	// define http server
 	server := &http.Server{
-		Addr: fmt.Sprintf(":%s", webPort),
+		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
 	}
 
@@ -25,5 +31,19 @@ func main() {
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	return Mail{
+		Domain:   os.Getenv("MAIL_DOMAIN"),
+		Host:     os.Getenv("MAIL_HOST"),
+		Port:     port,
+		Username: os.Getenv("MAIL_USERNAME"),
+		Password: os.Getenv("MAIL_PASSWORD"),
+		Encryption: os.Getenv("MAIL_ENCRYPTION"),
+		FromName: os.Getenv("MAIL_FROM_NAME"),
+		FromAddress: os.Getenv("MAIL_FROM_ADDRESS"),
 	}
 }
